5b4b05cced1b80174d3199368c681aa4b3fdb34f
bryce-bardwell <bryce.bardwell@sky.uk>
2025-10-19 16:25:45 +0100

    Add test settings and begin adding tests

--- backend/src/api/urls.py
+++ backend/src/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import health_check, vibe_check
 
 urlpatterns = [
-    path('ping', health_check),
-    path('vibe', vibe_check)
+    path('ping', health_check, name='health_check'),
+    path('vibe', vibe_check, name='vibe_check')
 ]
