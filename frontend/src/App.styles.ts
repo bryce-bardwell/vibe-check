41f2cebc33655b7cc46c908b4d39abc40806c3e6
bryce-bardwell <bryce.bardwell@sky.uk>
2025-12-16 20:39:08 +0000

    Add styled components

---
+++ frontend/src/App.styles.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const AppContainer = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  gap: 5%;
+  justify-content: center;
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
+  gap: 2%;
+`;
+
+export const ResultsContainer = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 50%;
+`;
